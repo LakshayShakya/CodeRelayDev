21cc7c99389d20f2a1a4bbd03cf42b29667050a5
lakrasumit <lakrasumit633@gmail.com>
2026-02-20 14:28:51 +0530

    Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,14 @@ import {
   Navigate,
 } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
+import Dashboard from "./components/auth/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/" element={<Navigate to="/auth" replace />} />
       </Routes>
     </Router>
